be2d35faac9a4bd62b3423a9a5fb4ae9016276f6
YANI018 <71872723+YANI018@users.noreply.github.com>
2022-10-06 11:10:23 +0000

    fgng

--- Propulsor/Assets/Scripts/PlayerController.cs
+++ Propulsor/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,25 +11,67 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float impulse = 2f;
 
+    [SerializeField]
+    TextMeshProUGUI labelFuel;
 
-    // Start is called before the first frame update
+    float actualFuel;
+
+    [SerializeField]
+    GameObject prefabParticles;
+
+    [SerializeField]
+    TextMeshProUGUI 
+
+
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-       
+            
+        actualFuel = 1f;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         direction.x = Input.GetAxis("Horizontal") * Time.deltaTime * impulse;
         direction.y = Input.GetAxis("Vertical") * Time.deltaTime * impulse;
 
-    }
+        actualFuel = actualFuel - 0.5f * Time.deltaTime;
+        labelFuel.text = actualFuel.ToString("00.00") + " %";
+
+        if (actualFuel <= 0f)
+        {
+            this.enabled = false;
+            youDied.SetActive(true);
+            textyouDied.text =
+        }
+
+}
 
     private void FixedUpdate()
     {
         body.AddForce(direction, ForceMode2D.Impulse);
 
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Fuel")
+        {
+            actualFuel += 10f;
+            if (actualFuel > 100f)
+            {
+                actualFuel = 100f;
+            }
+            Destroy(collision.gameObject);
+
+            //Crear particulas
+            Instantiate(prefabParticles, collision.transform.position, collision.transform.rotation);
+
+            //Destruir
+            Destroy(collision.gameObject);
+        }
+    }
 }
+
